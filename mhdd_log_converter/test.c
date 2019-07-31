64f191d24e39592b3f054478e161dbb6b8817c6e
ssrlive <ssrlivebox@gmail.com>
2019-07-31 11:37:17 +0800

    diskgenius log format added.

--- mhdd_log_converter/test.c
+++ mhdd_log_converter/test.c
@@ -3,15 +3,27 @@
 #include<assert.h>
 #include<string.h>
 
+void deal_with_mhdd_log(void);
+void deal_with_diskgenius_log(void);
+
 int main(int argc, const char *argv[]) {
+#if 0
+    deal_with_mhdd_log();
+#else
+    deal_with_diskgenius_log();
+#endif
+    system("PAUSE");
+    return 0;
+}
+
+void deal_with_mhdd_log(void) {
     char buffer[256] = { 0 };
     int count = 0;
     FILE *fp = fopen("mhdd.log","r");
     FILE *fp2 = fopen("badlist.txt","w");
     if (fp == NULL) {
-        printf("未找到 mhdd.log 文件！\n");
-        system("PAUSE");
-        exit(0);
+        printf("Can NOT find file \"mhdd.log\".\n");
+        return;
     }
     assert( fp2 != NULL);
     while( fgets(buffer, sizeof(buffer), fp) != NULL ) {
@@ -29,6 +41,33 @@ int main(int argc, const char *argv[]) {
     printf("count = %d\n", count );
     fclose( fp );
     fclose( fp2 );
-    system("PAUSE");
-    return 0;
+}
+
+void deal_with_diskgenius_log(void) {
+#define SECTORS_PER_CYLINDER 16065 /* 每柱面上的扇区数 */
+    char buffer[256] = { 0 };
+    int count = 0;
+    FILE *fp = fopen("diskgenius.txt","r");
+    FILE *fp2 = fopen("badlist.txt","w");
+    if (fp == NULL) {
+        printf("Can NOT find file \"diskgenius.txt\".\n");
+        return;
+    }
+    assert(fp2 != NULL);
+    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+        char buffer2[256] = { 0 };
+        long long x = 0;
+        char *p = buffer;
+        sscanf(p,"%lld", &x);
+        if (x > 0) {
+            int i = 0;
+            for (i = 0; i < SECTORS_PER_CYLINDER; ++i) {
+                fprintf(fp2, "%lld\n", x * SECTORS_PER_CYLINDER + i);
+            }
+            count++;
+        }
+    }
+    printf("count = %d\n", count);
+    fclose(fp);
+    fclose(fp2);
 }
